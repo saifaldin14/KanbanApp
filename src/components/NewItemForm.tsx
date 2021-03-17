1cdd3e6266c2d7d733b2e6ce486b43dc3a554918
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-17 07:38:19 -0400

    Update NewItemForm.tsx

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "../styles";
-const NewItemForm = () => {
-  return <div></div>;
+
+interface NewItemFormProps {
+  onAdd(text: string): void; // callback passed through AddNewItemProps
+}
+
+const NewItemForm = (props: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  const { onAdd } = props;
+
+  return (
+    <NewItemFormContainer>
+      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
 };
 
 export default NewItemForm;

808339e739de3757a6b525de64fe6a716a348ae7
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-16 21:53:19 -0400

    Update Column.tsx

---
+++ src/components/Column.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ColumnContainer } from "../styles";
+
+interface ColumnProps {
+  text: string;
+}
+const Column = () => {
+  return <div></div>;
+};
+
+export default Column;

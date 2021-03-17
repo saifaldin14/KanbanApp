5321a7345544841547895461c44d1d8ed793f806
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-17 07:51:21 -0400

    Update NewItemForm.tsx

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "../styles";
+import { useFocus } from "../utils/useFocus";
 
 interface NewItemFormProps {
   onAdd(text: string): void; // callback passed through AddNewItemProps
@@ -7,11 +8,16 @@ interface NewItemFormProps {
 
 const NewItemForm = (props: NewItemFormProps) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
   const { onAdd } = props;
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
 
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>

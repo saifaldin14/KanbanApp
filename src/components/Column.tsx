6d8b716f128953d3f7accf801ecb52b95412c554
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-17 07:44:44 -0400

    Update Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
+import AddNewItem from "./AddNewItem";
 
 interface ColumnProps {
   text: string;
@@ -9,6 +10,12 @@ const Column = ({ text, children }: React.PropsWithChildren<ColumnProps>) => {
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+
+      <AddNewItem
+        toggleButtonText="+ Add another task"
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   );
 };

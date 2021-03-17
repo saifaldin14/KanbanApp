d368ee775b0e37823b81bf1b0810d46cef40b43b
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-17 07:50:02 -0400

    Update useFocus.ts

--- src/utils/useFocus.ts
+++ src/utils/useFocus.ts
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from "react";
 
 export const useFocus = () => {
+  // use the useRef hook to get access to the rendered input element
   const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {

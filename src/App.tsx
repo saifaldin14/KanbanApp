5520337071323ce709f08afd1b828ad514cb3ada
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-16 22:02:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import { AppContainer } from "./styles";
+import Card from "./components/Card";
+import Column from "./components/Column";
+
 import "./App.css";
 
 function App() {
   return (
     <AppContainer>
-      <AppContainer></AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
     </AppContainer>
   );
 }

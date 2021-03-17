d46074527ce5844a75e6aebfff816c1831a39791
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-17 08:18:04 -0400

    Update Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
+import { useAppState } from "../AppStateContext";
 import AddNewItem from "./AddNewItem";
+import Card from "./Card";
 
 interface ColumnProps {
   text: string;
   index: number;
 }
-const Column = ({ text, children }: React.PropsWithChildren<ColumnProps>) => {
+const Column = ({ text, index }: ColumnProps) => {
+  const { state } = useAppState();
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
 
       <AddNewItem
         toggleButtonText="+ Add another task"

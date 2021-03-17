eedcaea8beb51e81a248c3087386609857a90906
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-16 21:56:49 -0400

    Update Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -4,10 +4,11 @@ import { ColumnContainer, ColumnTitle } from "../styles";
 interface ColumnProps {
   text: string;
 }
-const Column = ({ text }: ColumnProps) => {
+const Column = ({ text, children }: React.PropsWithChildren<ColumnProps>) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };

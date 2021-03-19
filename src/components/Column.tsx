26879b754b04468cf38445a547dbfcbb61766d2a
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-19 18:46:43 -0400

    Update Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,11 @@
 import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 import { ColumnContainer, ColumnTitle } from "../styles";
 import { useAppState } from "../AppStateContext";
 import { useItemDrag } from "../hooks/useItemDrag";
 import AddNewItem from "./AddNewItem";
 import Card from "./Card";
+import { DragItem } from "../utils/DragItem";
 
 interface ColumnProps {
   text: string;
@@ -12,6 +14,20 @@ interface ColumnProps {
 }
 const Column = ({ text, index, id }: ColumnProps) => {
   const { state, dispatch } = useAppState();
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover(item: DragItem) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      dispatch({ type: "MOVE_LIST", payload: { dragIndex, hoverIndex } });
+      item.index = hoverIndex;
+    },
+  });
   const ref = useRef<HTMLDivElement>(null);
   const { drag } = useItemDrag({ type: "COLUMN", id, index, text });
   drag(ref);

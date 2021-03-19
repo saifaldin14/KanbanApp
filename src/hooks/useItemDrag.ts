fcbb0792f820003ef11cbd18c9a2c15f4431a8d1
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-19 12:10:46 -0400

    Update useItemDrag.ts

--- src/hooks/useItemDrag.ts
+++ src/hooks/useItemDrag.ts
@@ -1,3 +1,20 @@
 import { useDrag } from "react-dnd";
 import { useAppState } from "../AppStateContext";
 import { DragItem } from "../utils/DragItem";
+
+export const useItemDrag = (item: DragItem) => {
+  const { dispatch } = useAppState();
+  const [, drag] = useDrag({
+    item,
+    begin: () =>
+      dispatch({
+        type: "SET_DRAGGED_ITEM",
+        payload: item,
+      }),
+    end: () =>
+      dispatch({
+        type: "SET_DRAGGED_ITEM",
+        payload: undefined,
+      }),
+  });
+};

b8b7d861c45d2d0ff7691108185067d9f86e80f6
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-19 18:56:23 -0400

    Update findItemIndexById.ts

--- src/utils/findItemIndexById.ts
+++ src/utils/findItemIndexById.ts
@@ -1,7 +1,7 @@
-import React from "react";
-
 interface Item {
   id: string;
 }
 
-export const findItemIndexById = <T extends Item>(items: T[], id: string) => {};
+export const findItemIndexById = <T extends Item>(items: T[], id: string) => {
+  return items.findIndex((item: T) => item.id === id);
+};

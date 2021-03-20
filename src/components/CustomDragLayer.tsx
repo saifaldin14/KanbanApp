66bfbd61d4eea7020a24cb9cc0b029194f6e898a
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-19 20:29:29 -0400

    Update CustomDragLayer.tsx

--- src/components/CustomDragLayer.tsx
+++ src/components/CustomDragLayer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { useDragLayer } from "react-dnd";
+import { XYCoord, useDragLayer } from "react-dnd";
 import { Column } from "./Column";
 import { CustomDragLayerContainer } from "../styles";
 
+function getItemStyles(currentOffset: XYCoord | null): React.CSSProperties {
+  if (!currentOffset) {
+    return {
+      display: "none",
+    };
+  }
+
+  const { x, y } = currentOffset;
+  const transform = `translate(${x}px, ${y}px)`;
+  return {
+    transform,
+    WebkitTransform: transform,
+  };
+}
 const CustomDragLayer: React.FC = () => {
   const { isDragging, item } = useDragLayer((monitor) => ({
     item: monitor.getItem(),

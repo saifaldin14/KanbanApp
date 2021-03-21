85e31bcffbb5a0b5ac95b7d0c98cd7d2f1c94479
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-21 11:15:22 -0400

    Reverted back to old version

--- src/hooks/useItemDrag.ts
+++ src/hooks/useItemDrag.ts
@@ -7,13 +7,12 @@ import { getEmptyImage } from "react-dnd-html5-backend";
 export const useItemDrag = (item: DragItem) => {
   const { dispatch } = useAppState();
   const [, drag, preview] = useDrag({
-    type: "SET_DRAGGED_ITEM",
     item,
-    // begin: () =>
-    //   dispatch({
-    //     type: "SET_DRAGGED_ITEM",
-    //     payload: item,
-    //   }),
+    begin: () =>
+      dispatch({
+        type: "SET_DRAGGED_ITEM",
+        payload: item,
+      }),
     end: () => dispatch({ type: "SET_DRAGGED_ITEM", payload: undefined }),
   });
   useEffect(() => {

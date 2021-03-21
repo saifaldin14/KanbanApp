75418dff9fba87e461e069c77501125899d5acee
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-21 09:46:53 -0400

    isHidden update

--- src/hooks/useItemDrag.ts
+++ src/hooks/useItemDrag.ts
@@ -1,6 +1,6 @@
 import { useDrag } from "react-dnd";
-import { useAppState } from "./AppStateContext";
-import { DragItem } from "./DragItem";
+import { useAppState } from "../AppStateContext";
+import { DragItem } from "../DragItem";
 import { useEffect } from "react";
 import { getEmptyImage } from "react-dnd-html5-backend";
 
